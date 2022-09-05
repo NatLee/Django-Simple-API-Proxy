2b7df6ad965d0d7c5cbd2df9eaa2b0d2af95cd44
NatLee <natlee.work@gmail.com>
2022-09-06 02:22:42 +0800

    [update] change version

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ setuptools.setup(
         'Topic :: Software Development :: Build Tools',
 
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3 :: Only',
